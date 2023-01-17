83102899c45dfab9b33ad72d55fc32b157b974de
Dmitrii Okhlopkov <91318266+tmokkuss@users.noreply.github.com>
2023-01-17 10:57:08 +0300

    Week_1_Task_1

--- Task_1_unittest.py
+++ Task_1_unittest.py
@@ -5,10 +5,10 @@ from Task_1 import *
 
 class Task1Test(unittest.TestCase):
     def test_get_grade_and_name(self):
-        self.assertEqual(get_grade_and_name("1week/Task_1_folder/data.txt")[0], 'Котов Алексей,4\n')
+        self.assertEqual(get_grade_and_name("data.txt")[0], 'Котов Алексей,4\n')
 
     def test_alphabetical_sorting(self):
-        self.assertEqual(alphabetical_sorting("1week/Task_1_folder/data.txt")[0], f'Бакушкин Даниил,3\n')
+        self.assertEqual(alphabetical_sorting("data.txt")[0], f'Бакушкин Даниил,3\n')
 
 
 if __name__ == '__main__':
